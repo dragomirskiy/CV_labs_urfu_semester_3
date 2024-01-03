f29de0d8d92c60f9479b4340dcaeef3ecde1e3fd
dragomirskiy <dragomirskiy@yandex.ru>
2024-01-03 21:49:00 +0300

    Added all labs

---
+++ README.md
@@ -0,0 +1,26 @@
+# CV_works
+
+Computer Vision Homeworks
+
+## Repository structure
+
+```bash
+.
+├── README.md
+└── src
+    ├── HW1
+    │   ├── dataset
+    │   │   └── train
+    │   │       ├── imgs
+    │   │       └── masks
+    │   ├── main.ipynb
+    │   ├── main.py     # Fish segmentation with classic cv2
+    │   └── readme.md
+    ├── HW2
+    │   ├── HW2_Golubev_AA.ipynb    # CNN
+    │   └── models
+    └── HW3
+        ├── HW3_gan_Golubev_AA.ipynb    # GAN, WGAN+Clipping, WGAN-GP, CWGAN-GP
+        ├── img
+        └── models
+```
---
+++ src/HW1/main.py
@@ -0,0 +1,52 @@
+import cv2
+import glob
+import numpy as np
+import os.path as osp
+from argparse import ArgumentParser
+from utils.compute_iou import compute_ious
+
+
+def segment_fish(img):
+    """
+    This method should compute masks for given image
+    Params:
+        img (np.ndarray): input image in BGR format
+    Returns:
+        mask (np.ndarray): fish mask. should contain bool values
+    """
+    # YOUR CODE HERE
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img_hsv = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2HSV)
+
+    light_orange = np.array((0, 169, 150))
+    dark_orange = np.array((100, 255, 255))
+    light_white = np.array((70, 0, 200))
+    dark_white = np.array((145, 180, 255))
+
+    orange_mask = cv2.inRange(img_hsv, light_orange, dark_orange)
+    white_mask = cv2.inRange(img_hsv, light_white, dark_white)
+
+    result_mask = orange_mask + white_mask
+
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (79, 60))
+    closing = cv2.morphologyEx(result_mask, cv2.MORPH_CLOSE, kernel)
+    opening = cv2.morphologyEx(closing, cv2.MORPH_OPEN, kernel)
+    return opening
+
+
+if __name__ == "__main__":
+
+    parser = ArgumentParser()
+    parser.add_argument("--is_train", action="store_true")
+    args = parser.parse_args()
+    stage = 'train' # if args.is_train else 'test'
+    data_root = osp.join("dataset", stage, "imgs")
+    img_paths = glob.glob(osp.join(data_root, "*.jpg"))
+    len(img_paths)
+
+    masks = dict()
+    for path in img_paths:
+        img = cv2.imread(path)
+        mask = segment_fish(img)
+        masks[osp.basename(path)] = mask
+    print(compute_ious(masks, osp.join("dataset", stage, "masks")))
